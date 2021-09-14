ee5be836623511d9e3bcecaca0597eac9e09df11
DESKTOP-SSG2KFQ\matrix <manuvin6984@gmail.com>
2021-09-14 13:17:09 +0530

    adding table for test env.

--- script_4.sql
+++ script_4.sql
@@ -4,3 +4,10 @@ create table rec_events
  rec_event_reference varchar2(30),
  rec_event_src  varchar2(30)
 );
+
+create table rec_events_test
+(
+ rec_event_test_id number,
+ rec_event_reference varchar2(30),
+ rec_event_src  varchar2(30)
+);
