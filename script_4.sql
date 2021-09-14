743c4ac52c2f5ada492569c789f0fe0bc53f36e1
DESKTOP-SSG2KFQ\matrix <manuvin6984@gmail.com>
2021-09-14 13:28:46 +0530

    Adding primary keys to the tables as per review comment

--- script_4.sql
+++ script_4.sql
@@ -1,13 +1,14 @@
 create table rec_events
 (
- rec_event_id number,
+ rec_event_id number primary key ,
  rec_event_reference varchar2(30),
  rec_event_src  varchar2(30)
 );
 
 create table rec_events_test
 (
- rec_event_test_id number,
+ rec_event_test_id number  primary key,
  rec_event_reference varchar2(30),
  rec_event_src  varchar2(30)
 );
+
